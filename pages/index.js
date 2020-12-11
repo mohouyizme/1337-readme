13634a1455950176c459804612eca23ef0685b6f
Mohamed Ouyizme <mohouyizme@gmail.com>
2020-12-12 00:59:57 +0100

    ✨ Add logo to home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Logo from '../components/Logo'
+
 export default function HomePage() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <h1 className="text-xl font-bold">1337 Readme</h1>
+    <div className="max-w-screen-md mx-auto">
+      <div className="flex justify-center my-24">
+        <Logo />
+      </div>
     </div>
   )
 }
