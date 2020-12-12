624a93646fdf21903a1a46c82a340c09ef3d31e7
Mohamed Ouyizme <mohouyizme@gmail.com>
2020-12-12 18:24:33 +0100

    ✨ Create profile card generator

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Logo from '../components/Logo'
+import ProfileCardGenerator from '../components/ProfileCardGenerator'
 
 export default function HomePage() {
   return (
     <div className="max-w-screen-md mx-auto">
-      <div className="flex justify-center my-24">
+      <div className="flex flex-col items-center my-24">
         <Logo />
       </div>
+      <ProfileCardGenerator />
     </div>
   )
 }
